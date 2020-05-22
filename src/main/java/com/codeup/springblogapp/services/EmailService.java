cb6774e886700ea5cd79278d525bad13a72ff3ba
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-05-22 00:14:17 -0500

    adjusted nav bar options

--- src/main/java/com/codeup/springblogapp/services/EmailService.java
+++ src/main/java/com/codeup/springblogapp/services/EmailService.java
@@ -1,6 +1,5 @@
 package com.codeup.springblogapp.services;
 
-
 import com.codeup.springblogapp.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
